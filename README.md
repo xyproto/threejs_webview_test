3ebf427341ff79ccfd36cc3ef724628944e65e56
Alexander F Rødseth <rodseth@gmail.com>
2018-05-07 11:31:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # threejs_webview_test
-Testing the combination of webview and threejs
+
+Testing the combination of webview and threejs.
+
+This package uses:
+
+* [webview](https://github.com/zserge/webview) in combination with
+* [onthefly](https://github.com/xyproto/onthefly) and
+* [threejs](https://threejs.org/)
+
+WebGL does not seem to work in webview yet, so there will only be a white window showing up.
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/urfave/negroni"
+	"github.com/xyproto/onthefly"
+	"github.com/zserge/webview"
+)
+
+// Create a Three.JS page
+func ThreeJSPage() *onthefly.Page {
+	p, t := onthefly.NewThreeJSWithGiven("Embedded Three.js", threejsSrc)
+
+	// Add a camera at (0, 0, 5)
+	t.AddCamera()
+	t.CameraPos("z", 5)
+
+	// We also need a renderer
+	t.AddRenderer()
+
+	// Create a test cube
+	cube1 := t.AddTestCube()
+
+	// Create another test cube, and rotate it a bit
+	cube2 := t.AddTestCube()
+	cube2.JS += cube2.ID + ".rotation.x += 0.9;"
+
+	// Render function (happens every frame)
+	r := onthefly.NewRenderFunction()
+
+	// Rotate the first cube
+	r.AddJS(cube1.ID + ".rotation.x += 0.02;")
+	r.AddJS(cube1.ID + ".rotation.y += 0.02;")
+
+	// Rotate the second cube at a different speed
+	r.AddJS(cube2.ID + ".rotation.x += 0.04;")
+	r.AddJS(cube2.ID + ".rotation.y += 0.07;")
+
+	// Add the render function to the script tag
+	t.AddRenderFunction(r, true)
+
+	return p
+}
+
+func main() {
+
+	// Create a Negroni instance and a ServeMux instance
+	n := negroni.Classic()
+	mux := http.NewServeMux()
+
+	// Create the page by calling the function above
+	page := ThreeJSPage()
+
+	// Publish the generated page (HTML and CSS)
+	page.Publish(mux, "/", "/style.css", false)
+
+	// Handler goes last
+	n.UseHandler(mux)
+
+	// Listen for requests at port 1814
+	go func() {
+		n.Run(":1814")
+	}()
+
+	// Open three.js 3D graphics in a 1024x768 resizable window
+	webview.Open("Three JS", "http://localhost:1814/", 1024, 768, true)
+}
